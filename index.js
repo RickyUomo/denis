5b5bdf32283ec234b2f8cd557a3ff5b7601c6c52
Ricky Kuo <rickykuo925@gmail.com>
2022-03-11 15:01:21 +0000

    go to questionaire

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
+const popUp = document.getElementById('pop-up');
+const closeBtn = document.getElementById('close-btn');
 
-function timeOut() {
+setTimeout(() => {
+    popUp.classList.remove('hide');
+    popUp.classList.add('show');
+}, 3000)
 
-}
-
+closeBtn.addEventListener('click', () => window.location.replace("https://docs.google.com/forms/d/e/1FAIpQLSc9urrrKWGUP9Wu8bEzUsc-ik-WbnEFgo_kJ2Ewz7F_J5A2cQ/viewform"))
