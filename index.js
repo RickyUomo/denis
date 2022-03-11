769bc96fe03a65d2eb67ea87e9864d46e277d0ab
Ricky Kuo <rickykuo925@gmail.com>
2022-03-11 17:20:19 +0000

    changeed time

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ const closeBtn = document.getElementById('close-btn');
 setTimeout(() => {
     popUp.classList.remove('hide');
     popUp.classList.add('show');
-}, 3000)
+}, 180000)
 
 closeBtn.addEventListener('click', () => window.location.replace("https://docs.google.com/forms/d/e/1FAIpQLSc9urrrKWGUP9Wu8bEzUsc-ik-WbnEFgo_kJ2Ewz7F_J5A2cQ/viewform"))
