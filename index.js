7dfa05e9c1ebf4ac3d8fd665fd3d391e4291864c
Ricky Kuo <rickykuo925@gmail.com>
2022-03-14 12:07:52 +0000

    small chianges

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 const popUp = document.getElementById('pop-up');
-const closeBtn = document.getElementById('close-btn');
 
 setTimeout(() => {
     popUp.classList.remove('hide');
     popUp.classList.add('show');
-}, 180000)
-
-closeBtn.addEventListener('click', () => window.location.replace("https://docs.google.com/forms/d/e/1FAIpQLSc9urrrKWGUP9Wu8bEzUsc-ik-WbnEFgo_kJ2Ewz7F_J5A2cQ/viewform"))
+}, 100000)
